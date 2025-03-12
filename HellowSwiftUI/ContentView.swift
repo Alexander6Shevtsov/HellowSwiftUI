f6d918f779b869d7765a312f48c2995f29b46cb9
Alexander6Shevtsov <axrsvr@gmail.com>
2025-03-12 22:58:41 +0300

    Finished

--- HellowSwiftUI/ContentView.swift
+++ HellowSwiftUI/ContentView.swift
@@ -6,13 +6,15 @@
 //
 
 import SwiftUI
-    // основной экран приложения
+// основной экран приложения
 struct ContentView: View { // подписываем под View
-    var body: some View { // вычисляемое свойство возвращает какую то View
-        HStack { // правой кн то Text Embed in HStack
-            Image(systemName: "swift")
-                .font(.largeTitle)
-                .foregroundColor(.orange)
+    @State private var tapCount = 0 // @State помечается то что меняет состояние в интерфейсе
+    
+    var body: some View { // вычисляемое свойство возвращает какую-то View
+        VStack {
+            SwiftLogoView(color: .orange)
+                .padding(.bottom, -10) // придвинуть текст выше
+            
             VStack(alignment: .leading) { // правой кн то Text Embed in VStack
                 Text("Hello, SwiftUI!") // чтоб под ним разместить еще текст
                     .font(.title)
@@ -23,10 +25,28 @@ struct ContentView: View { // подписываем под View
                     Spacer() // раздвигает элементы
                     Text("Lesson One")
                 }
+                
+                Spacer()
             }
-        }
-        .padding() // добавляет отступы везде по 16
+            
+//            Button("Tap count: \(tapCount)") { //  кнопка без измен текста
+//                tapCount += 1
+//            }
+            Button(action: { tapCount += 1 }) { // чтобы считало, нужен приват метод после body...
+                Text("Tap count: \(tapCount)")
+                    .font(.title)
+            }
+            .padding()
+            .background(.blue) // фон кнопки
+            .foregroundColor(.white) // цвет шрифта
+            .clipShape(Capsule()) // скругление капсулой
+        } // ничего не указываем - везде по 16 | .padding(.top, 16) только верх
+        .padding(EdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16)) // добавляет отступы
     }
+    
+//    private func buttonAction() { !но если метод в одну строку -> сразу пишем её в {}
+//        tapCount += 1
+//    }
 }
 
 #Preview {
--- HellowSwiftUI/SwiftLogoView.swift
+++ HellowSwiftUI/SwiftLogoView.swift
@@ -8,16 +8,28 @@
 import SwiftUI
 
 struct SwiftLogoView: View {
+    let color: Color // чтоб не хардкодить цвет, создаем свойство...
+
     var body: some View {
-        Color.orange
-            .ignoresSafeArea() // игнорирует Safe Area
-            .frame(width: 250, height: 250) // набираем .framewh
-            .clipShape(Circle()) // обрезка по кругу
-            .overlay(Circle().stroke(Color.black, lineWidth: 4)) //выделение границ
-        
+//      Color.orange можно вместо круга скруглить само тело
+        ZStack {
+            Circle()
+            //          .ignoresSafeArea() // игнорирует Safe Area
+                .foregroundColor(color) // цвет круга
+                .frame(width: 250, height: 250) // набираем .framewh
+            //          .clipShape(Circle()) // обрезка по кругу
+                .overlay(Circle().stroke(Color.white, lineWidth: 2)) //выделение границ белым толщь 1
+                .shadow(radius: 8) // тень
+            
+            Image(systemName: "swift")
+                .resizable() // если меняется не размер картинки а его рамка
+                .foregroundColor(.white)
+                .frame(width: 150, height: 150)
+                .offset(x: -10, y: -10) // выравниваем по осям
+        }
     }
 }
 
 #Preview {
-    SwiftLogoView()
+    SwiftLogoView(color: .orange) // ... и иниц его тут
 }
